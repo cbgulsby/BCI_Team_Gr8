b4738db184c3b6df42216f110625a67acc7164c6
Nick (Macbook) <nicksayswatzup@gmail.com>
2019-04-13 15:23:55 -0500

    nick's changes into bailey's branch (proper architecture)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from "react"
+import React from "react";
+
+import { Line, Bar } from "react-chartjs-2";
+
+import "./styles.css";
+import "./stylesheets/index.css";
+
+var stressLevelNormal = true;
 
 function App() {
-    return (
-      <div className="App">
-        <h1>In The Zone</h1>
+  return (
+    <div className="App">
+      <div className="header-background">
+        <div className="header-text">In The Zone</div>
+      </div>
+      <div className={ stressLevelNormal ? 'normal' + ' app-body' : 'not-normal' + ' app-body'}>
         <h2>Start playing to see some magic happen!</h2>
       </div>
-    );
+    </div>
+  );
 }
 
 export default App
