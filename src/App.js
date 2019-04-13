eb2219090d77902ab0511d9203708d52ea3fef2c
Bailey <cbgulsby@crimson.ua.edu>
2019-04-13 15:17:59 -0500

    changed file system

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function App() {
+    return (
+      <div className="App">
+        <h1>In The Zone</h1>
+        <h2>Start playing to see some magic happen!</h2>
+      </div>
+    );
+}
+
+export default App
---
+++ src/music_player.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function music_player() {
+    return(
+        <div>
+            <h1>I made something appear</h1>
+        </div>
+    );
+}
+
+export default music_player
