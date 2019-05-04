a63a41896f887aeda4fc09df2a5aaf62bcff40c7
Bailey <cbgulsby@crimson.ua.edu>
2019-05-03 19:37:14 -0500

    updated MusicPlayer to play one mp3

--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -2,13 +2,21 @@ import React from 'react';
 import sound1 from "../assets/harryPotter.mp3";
 import sound2 from "../assets/starWars.mp3";
 import "../stylesheets/MusicPlayer.styles.css";
+import AudioPlayer from "react-h5-audio-player";
 
 let song;
 
 class MusicPlayer extends React.Component {
     
-    song = "sound1";
+    // song = sound1;
     
+    // updateSource()
+    // {
+    //     var playMe = document.getElementById('playMe');
+    //     playMe.src=sound1;
+    //     playMe.onload();
+    // }
+
     changeSong()
     {
         console.log("here");
@@ -26,17 +34,21 @@ class MusicPlayer extends React.Component {
 
     render() {
         return (
-            <div className="MusicPlayer">
-                <button onClick={this.changeSong}>ACTIVATE</button>
-                <br/>
-                <br/>
-                <audio id="myAudio" controls className={song === "sound1" ? "" : "hidden"}>
-                    <source src={sound1} type="audio/mp3" />
-                </audio>
-                <audio id="myAudio2" controls className={song === "sound2" ? "" : "hidden"}>
-                    <source src={sound2} type="audio/mp3" />
-                </audio>
-            </div>
+            <AudioPlayer
+            src={sound1}
+            onListen={this.changeSong}
+            />
+            // <div className="MusicPlayer">
+            //     <button onClick={this.changeSong}>ACTIVATE</button>
+            //     <br/>
+            //     <br/>
+            //     <audio id="myAudio" controls className={song === "sound1" ? "" : "hidden"}>
+            //         <source src={sound1} type="audio/mp3" />
+            //     </audio>
+            //     <audio id="myAudio2" controls className={song === "sound2" ? "" : "hidden"}>
+            //         <source src={sound2} type="audio/mp3" />
+            //     </audio>
+            // </div>
         );
     }
 }
